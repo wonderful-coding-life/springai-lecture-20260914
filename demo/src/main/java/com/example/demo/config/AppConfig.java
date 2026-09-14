9b09408aab1cc6894791337f38e8b9ee7ce11cbd
tutor <wonderful.coding.life@gmail.com>
2026-09-14 14:46:16 +0900

    added chatclient

--- demo/src/main/java/com/example/demo/config/AppConfig.java
+++ demo/src/main/java/com/example/demo/config/AppConfig.java
@@ -1,13 +1,24 @@
 package com.example.demo.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+    public ChatClient chatClient(ChatClient.Builder builder, ChatMemory chatMemory) {
+        return builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultOptions(OpenAiChatOptions.builder()
+                                .model("gpt-5.6-luna")
+                                .reasoningEffort("none") // none, low, medium, high, xhigh, max
+                                .serviceTier("default") // default, flex, priority
+                                //.temperature(1.0)
+                )
+                .build();
     }
 }
--- demo/src/main/java/com/example/demo/controller/ApiController.java
+++ demo/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,20 +1,27 @@
 package com.example.demo.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
-
     @Autowired
     private ChatClient chatClient;
 
     @PostMapping("/chats")
-    public String postChats(@RequestBody String message) {
+    public String postChats(@RequestBody String message, @RequestParam("conversationId") String conversationId) {
         return chatClient.prompt()
+                .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, conversationId))
+                .system("마크다운 형식을 사용하지 말고 순수한 텍스트 형식으로 답 해 주세요.")
                 .user(message)
                 .call().content();
     }
